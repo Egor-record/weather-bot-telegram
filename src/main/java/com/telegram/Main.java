f8837a6a4d329b3067996e9de513a147428134b5
George Siaskov <egor.record@yandex.ru>
2020-01-08 21:41:24 +0300

    Gets messages from Telegram with different API

--- src/main/java/com/telegram/Main.java
+++ src/main/java/com/telegram/Main.java
@@ -1,14 +1,33 @@
 package com.telegram;
 
-import org.telegram.telegrambots.meta.TelegramBotsApi;
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Message;
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.GetUpdates;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.GetUpdatesResponse;
+import com.pengrad.telegrambot.response.SendResponse;
+
+import java.util.List;
+
 
 public class Main {
 
         public static void main(String[] args) throws Exception {
 
-            Weather weather = new Weather();
+          //  Weather weather = new Weather();
 
-            weather.getRequestFromAPI();
+           // weather.getRequestFromAPI();
+
+            TelegramBot bot = new TelegramBot(Keys.Telegram_Key);
+
+            GetUpdates getUpdates = new GetUpdates().limit(100).offset(0).timeout(0);
+
+            GetUpdatesResponse updatesResponse = bot.execute(getUpdates);
+
+            System.out.println(updatesResponse);
+
 
         }
 }
