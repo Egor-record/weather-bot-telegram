b9e18d8c759529155d81549bb92d92030a68269a
George Siaskov <egor.record@yandex.ru>
2020-01-12 19:18:08 +0300

    Recieve and send messages to bot

--- src/main/java/com/telegram/Main.java
+++ src/main/java/com/telegram/Main.java
@@ -1,13 +1,10 @@
 package com.telegram;
 
-import com.pengrad.telegrambot.TelegramBot;
-import com.pengrad.telegrambot.UpdatesListener;
-import com.pengrad.telegrambot.model.Message;
-import com.pengrad.telegrambot.model.Update;
-import com.pengrad.telegrambot.request.GetUpdates;
-import com.pengrad.telegrambot.request.SendMessage;
-import com.pengrad.telegrambot.response.GetUpdatesResponse;
-import com.pengrad.telegrambot.response.SendResponse;
+
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.util.List;
 
@@ -20,13 +17,15 @@ public class Main {
 
            // weather.getRequestFromAPI();
 
-            TelegramBot bot = new TelegramBot(Keys.Telegram_Key);
+            ApiContextInitializer.init();
 
-            GetUpdates getUpdates = new GetUpdates().limit(100).offset(0).timeout(0);
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
 
-            GetUpdatesResponse updatesResponse = bot.execute(getUpdates);
-
-            System.out.println(updatesResponse);
+            try {
+                telegramBotsApi.registerBot(new Weather_bot());
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
 
 
         }
--- src/main/java/com/telegram/Weather_bot.java
+++ src/main/java/com/telegram/Weather_bot.java
@@ -1,7 +1,9 @@
 package com.telegram;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 
 public class Weather_bot extends TelegramLongPollingBot {
@@ -12,6 +14,18 @@ public class Weather_bot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
         System.out.println(update.getMessage().getText());
+
+        SendMessage message = new SendMessage();
+
+        message.setText("Hello");
+
+        message.setChatId(update.getMessage().getChatId());
+
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
